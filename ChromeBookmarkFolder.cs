2a34ec7bc39717e5eb21a7757629a087a76b82ac
sapph42 <nick.gibson@gmail.com>
2023-10-04 22:49:41 -0400

    Initial commit

---
+++ ChromeBookmarkFolder.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+#nullable enable
+namespace ChromeBookmarkMerge {
+    internal class ChromeBookmarkFolder {
+#pragma warning disable IDE1006 // Naming Styles
+        public List<object>? children { get; set; } = null;
+        public string? date_added { get; set; } = null;
+        public string? date_last_used { get; set; } = null;
+        public string? date_modified { get; set; } = null;
+        public string? guid { get; set; } = null;
+        public string? id { get; set; } = null;
+        public string? name { get; set; } = null;
+        public string? type { get; set; } = null;
+        private static string? UlongStringMax(string? a, string? b) {
+            if (a is null && b is null)
+                return null;
+            if (a is null)
+                return b;
+            if (b is null)
+                return a;
+            return UInt64.Parse(a) > UInt64.Parse(b) ? a : b;
+        }
+#pragma warning restore IDE1006 // Naming Styles
+        public void ImportJToken(JToken token) {
+            date_added = token.Value<string>("date_added");
+            date_last_used = token.Value<string>("date_last_used");
+            date_modified = token.Value<string>("date_modified");
+            guid = token.Value<string>("guid");
+            id = token.Value<string>("id");
+            name = token.Value<string>("name");
+            type = "folder";
+            children = new List<object>();
+            JToken target = token.SelectToken("$.children")!;
+            //if (target.SelectToken("$Value") is null)
+            //    target = target.SelectToken("$[0]")!;
+            foreach (JToken child in target) {
+                if (child.Value<string>("type") == "url")
+                    children.Add(child.ToObject<ChromeBookmarkItem>()!);
+                if (child.Value<string>("type") == "folder") {
+                    ChromeBookmarkFolder newFolder = new ChromeBookmarkFolder();
+                    newFolder.ImportJToken(child);
+                    children.Add(newFolder);
+                }
+            }
+        }
+        public JToken ExportJToken() {
+            return JToken.FromObject(this);
+        }
+        public void Union(ChromeBookmarkFolder other, string value="live") {
+            Debug.WriteLine($"Union started on {name}");
+            string childFolderNamesJoined = string.Join(",", other.children.Where(c => c.GetType().Equals(typeof(ChromeBookmarkFolder))).Cast<ChromeBookmarkFolder>().Select(f => f.name).ToArray());
+            if (other.name != name)
+                return;
+            date_modified = UlongStringMax(date_modified, other.date_modified);
+            if (other.children is null)
+                return;
+            children ??= new List<object>(); //if children is null, instantiate
+            foreach (object child in other.children) {
+                if (child.GetType().Equals(typeof(ChromeBookmarkItem))) {
+                    if (!children.Contains(child))
+                        children.Add(child);
+                }
+                if (child.GetType().Equals(typeof(ChromeBookmarkFolder))) {
+                    Debug.WriteLine($"Testing {value} folder: {((ChromeBookmarkFolder)child).name}");
+                    foreach (object localchild in children) {
+                        if (localchild.GetType().Equals(typeof(ChromeBookmarkItem)))
+                            continue;
+                        Debug.WriteLine($" -- Against backup folder: {((ChromeBookmarkFolder)localchild).name}");
+                        ChromeBookmarkFolder templocal = (ChromeBookmarkFolder)localchild;
+                        ChromeBookmarkFolder tempother = (ChromeBookmarkFolder)child;
+                        if (templocal.name == tempother.name) {
+                            Debug.WriteLine($" ---- Union perform");
+                            ((ChromeBookmarkFolder)localchild).Union((ChromeBookmarkFolder)child);
+                        }
+                        continue;
+                    }
+                }
+            }
+            List<object> otherchildfolders = other.children.Where(c => c.GetType().Equals(typeof(ChromeBookmarkFolder))).ToList<object>();
+            if (otherchildfolders.Count == 0) {
+                Debug.WriteLine($"Union complete on {name} - 85");
+                return; //The other folder has no child folders are this level.  Merge unneccessary
+            }
+            List<ChromeBookmarkFolder> typedOtherFolders = otherchildfolders.Cast<ChromeBookmarkFolder>().ToList<ChromeBookmarkFolder>();
+            List<string?> otherFolderNames = typedOtherFolders.Select(f => f.name).ToList<string?>();
+            List<object> childfolders = children.Where(c => c.GetType().Equals(typeof(ChromeBookmarkFolder))).ToList<object>();
+            if (childfolders.Count == 0) {
+                // this folder has no child folders at this level. Absorb all child folders from other folder.
+                foreach (ChromeBookmarkFolder folder in typedOtherFolders) {
+                    Debug.WriteLine($"Copying {folder.name} to {name}");
+                    children.Add(folder);
+                }
+                Debug.WriteLine($"Union complete on {name} - 97");
+                return;
+            }
+            List<ChromeBookmarkFolder> typedFolders = childfolders.Cast<ChromeBookmarkFolder>().ToList<ChromeBookmarkFolder>();
+            List<string?> folderNames = typedFolders.Select(f => f.name).ToList<string?>();
+            List<string?> needsCopyingNames = otherFolderNames.Except(folderNames).ToList<string?>();
+            if (needsCopyingNames is null) {
+                Debug.WriteLine($"Union complete on {name} - 104");
+                return; //All folders match between both this and other folder.  Merge will be handled above.
+            }
+            IEnumerable<ChromeBookmarkFolder> needsCopyingFolders = typedOtherFolders.Where(c => needsCopyingNames.Contains(c.name));
+            foreach (ChromeBookmarkFolder folder in needsCopyingFolders) {
+                Debug.WriteLine($"Copying {folder.name} to {name}");
+                children.Add(folder);
+            }
+            Debug.WriteLine($"Union complete on {name} - 112");
+        }
+        public bool Equals(ChromeBookmarkFolder? other) {
+            if (other is null)
+                return false;
+            if (name != other.name)
+                return false;
+            if (children is null && other.children is null)
+                return true;
+            if (children is null ^ other.children is null)
+                return false;
+            if (children is null)
+                throw new NullReferenceException();
+            if (other.children is null)
+                throw new NullReferenceException();
+            if (children.Count == 0 && other.children.Count == 0)
+                return true;
+            foreach (ChromeBookmarkItem child in children.Where(c => c is ChromeBookmarkItem).Cast<ChromeBookmarkItem>()) {
+                if (!other.children.Contains(child))
+                    return false;
+            }
+            foreach (ChromeBookmarkItem child in other.children.Where(c => c is ChromeBookmarkItem).Cast<ChromeBookmarkItem>()) {
+                if (!children.Contains(child))
+                    return false;
+            }
+            foreach (ChromeBookmarkFolder child in children.Where(c => c is ChromeBookmarkFolder).Cast<ChromeBookmarkFolder>()) {
+                if (!other.children.Contains(child))
+                    return false;
+            }
+            foreach (ChromeBookmarkFolder child in other.children.Where(c => c is ChromeBookmarkFolder).Cast<ChromeBookmarkFolder>()) {
+                if (!children.Contains(child))
+                    return false;
+            }
+            return true;
+        }
+        public override bool Equals(object obj) => Equals(obj as ChromeBookmarkFolder);
+        public override int GetHashCode() {
+            if (children is null) {
+                if (name is null)
+                    return 0;
+                return name.GetHashCode();
+            }
+            return (name, children).GetHashCode();
+        }
+    }
+}
---
+++ ChromeBookmarkMerge.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ChromeBookmarkMerge {
+    public class ChromeBookmarkMerge {
+        private FileInfo bookmark_file_1;
+        private FileInfo bookmark_file_2;
+        public string InputFile1 {
+            get => InputFile1; 
+            set {
+                InputFile1 = value;
+                bookmark_file_1 = new FileInfo(value);
+            }
+        }
+        public string InputFile2 { 
+            get => InputFile2; 
+            set {
+                InputFile2 = value;
+                bookmark_file_2 = new FileInfo(value);
+            } 
+        }
+
+        public ChromeBookmarkMerge(string InputFile1, string InputFile2) {
+            this.InputFile1 = InputFile1;
+            this.InputFile2 = InputFile2;
+        }
+        public bool Merge(string Destination) {
+            if (!bookmark_file_1.Exists && !bookmark_file_2.Exists) {
+                return false;
+            }
+            FileInfo destination_file = new FileInfo(Destination);
+            if (!destination_file.Directory.Exists) {
+                return false;
+            }
+            foreach (Process chromeproc in Process.GetProcessesByName("chrome")) {
+                chromeproc.Kill();
+                chromeproc.WaitForExit();
+            }
+            if (!bookmark_file_1.Exists) {
+                bookmark_file_2.CopyTo(Destination);
+                return true;
+            }
+            if (!bookmark_file_2.Exists) {
+                bookmark_file_1.CopyTo(Destination);
+                return true;
+            }
+            try {
+                string file1_contents;
+                string file2_contents;
+                using (FileStream fileStream = bookmark_file_1.OpenRead()) {
+                    using StreamReader streamReader = new StreamReader(fileStream);
+                    file1_contents = streamReader.ReadToEnd();
+                }
+                using (FileStream fileStream = bookmark_file_2.OpenRead()) {
+                    using StreamReader streamReader = new StreamReader(fileStream);
+                    file2_contents = streamReader.ReadToEnd();
+                }
+                JObject file1_Json = JObject.Parse(file1_contents);
+                JObject file2_Json = JObject.Parse(file2_contents);
+                file1_Json.Remove("checksum");
+                file2_Json.Remove("checksum");
+                ChromeBookmarkFolder file1_bookmark_bar = new ChromeBookmarkFolder();
+                ChromeBookmarkFolder file1_other = new ChromeBookmarkFolder();
+                ChromeBookmarkFolder file2_bookmark_bar = new ChromeBookmarkFolder();
+                ChromeBookmarkFolder file2_other = new ChromeBookmarkFolder();
+                file1_bookmark_bar.ImportJToken(file1_Json.SelectToken("$.roots.bookmark_bar")!);
+                file1_other.ImportJToken(file1_Json.SelectToken("$.roots.other")!);
+                file2_bookmark_bar.ImportJToken(file2_Json.SelectToken("$.roots.bookmark_bar")!);
+                file2_other.ImportJToken(file2_Json.SelectToken("$.roots.other")!);
+                file2_bookmark_bar.Union(file1_bookmark_bar);
+                file2_other.Union(file1_other);
+                file2_Json.SelectToken("$.roots.bookmark_bar")!.Replace(file2_bookmark_bar.ExportJToken());
+                file2_Json.SelectToken("$.roots.other")!.Replace(file2_other.ExportJToken());
+                destination_file.Delete();
+                using (FileStream fileStream = destination_file.OpenWrite()) {
+                    byte[] data = new UTF8Encoding(true).GetBytes(file2_Json.ToString(Formatting.Indented));
+                    fileStream.Write(data, 0, data.Length);
+                }
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
+}
