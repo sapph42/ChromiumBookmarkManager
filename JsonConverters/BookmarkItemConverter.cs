f30addd4c7dc91bbff0d666a96adf4d0cb618f12
nick.gibson <31635854+sapph42@users.noreply.github.com>
2025-03-10 11:04:50 -0400

    Added custom deserialization logic

--- JsonConverters/BookmarkItemConverter.cs
+++ JsonConverters/BookmarkItemConverter.cs
@@ -2,17 +2,27 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
+#nullable enable
 namespace SapphTools.BookmarkManager.Chromium {
     internal class BookmarkItemConverter : JsonConverter<BookmarkItem> {
         public override BookmarkItem Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
             using JsonDocument doc = JsonDocument.ParseValue(ref reader);
             JsonElement root = doc.RootElement;
             if (root.TryGetProperty("type", out JsonElement typeElement)) {
-                string type = typeElement.GetString();
-                if (type == "folder")
-                    return JsonSerializer.Deserialize<BookmarkFolder>(root.GetRawText(), options);
-                else if (type == "url")
-                    return JsonSerializer.Deserialize<BookmarkUrl>(root.GetRawText(), options);
+                string type = typeElement.GetString() ?? "";
+                if (type == "folder") {
+                    BookmarkFolder? folder = JsonSerializer.Deserialize<BookmarkFolder>(root.GetRawText(), options);
+                    if (folder is null)
+                        throw new JsonException("Could not deserialize folder");
+                    else
+                        return folder;
+                } else if (type == "url") {
+                    BookmarkUrl? url = JsonSerializer.Deserialize < BookmarkUrl >(root.GetRawText(), options);
+                    if (url is null)
+                        throw new JsonException("Could not deserialize url");
+                    else
+                        return url;
+                }
             }
             throw new JsonException("Unknown bookmark item type");
         }
@@ -26,3 +36,4 @@ namespace SapphTools.BookmarkManager.Chromium {
         }
     }
 }
+#nullable disable
